733f59268f52747ac18201a5adfe6cac0e5a5a6d
liliankim <yejean831@gmail.com>
2022-09-18 22:01:17 -0700

    completed associations

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,36 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+//foreign key is parent information
+Product.belongsTo(Category,{
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
+//products and tags are like siblings -this is a way to connect them
+//do not have parent child relationship
+//trip is third table to connect
+Product.belongsToMany(Tag,{
+  through:{
+    model:ProductTag,
+    unique: false
+  }
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  through: {
+    model:ProductTag,
+    unique:false
+  },
 
+  
+})
 module.exports = {
   Product,
   Category,
